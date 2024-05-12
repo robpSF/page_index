72870c1dc1b110630afbefd7e5091fdb7396a532
robpSF <robert@tstoryteller.com>
2024-05-12 13:36:43 +0100

    Update streamlit_app.py
    need to make the page explicit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
 
 def create_html_index(url):
     try:
         # Fetching the webpage content
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract the base URL to append to IDs
+        parsed_url = urlparse(url)
+        base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
         # Searching for headings with an ID
         headings = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], id=True)
@@ -16,7 +21,9 @@ def create_html_index(url):
         for heading in headings:
             id = heading['id']
             text = heading.text.strip()
-            html_index += f'  <li><a href="#{id}">{text}</a> - Description of the section</li>\n'
+            # Create absolute URL for each link
+            link = urljoin(base_url, f'#{id}')
+            html_index += f'  <li><a href="{link}">{text}</a> - Description of the section</li>\n'
 
         html_index += '</ul>'
         return html_index
